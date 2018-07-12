7820b1a67c88e5fb04df631fb902f9e7850d6b70
Michael Quach <michaelquach@openarc.net>
2018-07-12 11:55:28 -0400

    setting up heroku

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const hbs = require("hbs");
 
 const fs = require("fs");
+
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname+'/views/partials');
@@ -55,6 +57,6 @@ app.get('/bad',(req,res)=>{
     });
 });
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("server is up on port 3000");
 });
